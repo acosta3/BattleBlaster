e3bc595288669fe9cf67293a7608257033640e49
acosta3 <acosta3@mcmaster.ca>
2025-11-26 14:40:37 -0500

    Finding the Actors in GameMode

--- Source/BattleBlaster/BattleBlasterGameMode.cpp
+++ Source/BattleBlaster/BattleBlasterGameMode.cpp
@@ -2,4 +2,25 @@
 
 
 #include "BattleBlasterGameMode.h"
+#include "Kismet/GameplayStatics.h"
+#include"Tower.h"
 
+void ABattleBlasterGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+
+	TArray<AActor*> Towers;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), ATower::StaticClass(), Towers);
+
+	TowerCount = Towers.Num();
+	UE_LOG(LogTemp, Warning, TEXT("Tower Count: %d"), TowerCount);	
+	
+	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(this, 0);
+	if (PlayerPawn) 
+	{
+		Tank = Cast<ATank>(PlayerPawn);
+	}
+	if (!Tank) {
+		UE_LOG(LogTemp, Warning, TEXT("Tank not found!"));
+	}
+}
