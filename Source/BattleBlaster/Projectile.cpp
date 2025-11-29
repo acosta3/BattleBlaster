0a84a3d6b33341843029bc476209d7c41119f9a7
Anthony Acosta <anthonycylecacosta@gmail.com>
2025-11-29 01:45:53 -0500

    pushing for now

--- Source/BattleBlaster/Projectile.cpp
+++ Source/BattleBlaster/Projectile.cpp
@@ -2,8 +2,9 @@
 
 
 
-
 #include "Projectile.h"
+
+#include "Kismet/GameplayStatics.h"
 
 
 // Sets default values
@@ -40,10 +41,19 @@ void AProjectile::Tick(float DeltaTime)
 
 void AProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
 {
-	if(OtherActor)
+	
+	
+	
+	AActor* MyOwner = GetOwner();
+	
+	if (MyOwner)
 	{
-		UE_LOG(LogTemp, Display, TEXT("Projectile hit: %s"), *OtherActor->GetActorNameOrLabel());
+		if (OtherActor && OtherActor != this && OtherActor != MyOwner)
+		{
+			UGameplayStatics::ApplyDamage(OtherActor, Damage, MyOwner->GetInstigatorController(), this, UDamageType::StaticClass());
+		}
+		
 	}
-
+	this->Destroy();
 }
 
