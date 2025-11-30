91f069b2b4d185a2b87218f7c7491d70226475de
Anthony Acosta <anthonycylecacosta@gmail.com>
2025-11-30 07:53:24 -0500

    Widget is Properly working now

--- Source/BattleBlaster/BattleBlasterGameMode.cpp
+++ Source/BattleBlaster/BattleBlasterGameMode.cpp
@@ -45,8 +45,17 @@ void ABattleBlasterGameMode::BeginPlay()
 			}
 			LoopIndex++;
 		}
-
-	
+		 APlayerController* PlayerController = UGameplayStatics::GetPlayerController(GetWorld(), 0);
+		 if (PlayerController)
+		 {
+			 ScreenMessageWidget = CreateWidget<UScreenMessage>(PlayerController, ScreenMessageClass);
+			 if(ScreenMessageClass)
+			 {
+				 ScreenMessageWidget->AddToPlayerScreen();
+				 ScreenMessageWidget->SetMessageText("Get Ready!");
+			 }
+		 }
+		
 		CountdownSeconds = CountdownDelay;
 		GetWorldTimerManager().SetTimer(CountdownTimerHandle, this, &ABattleBlasterGameMode::OnCountdownTimeTimeout, 1.0f, true);
 
@@ -58,17 +67,18 @@ void ABattleBlasterGameMode::OnCountdownTimeTimeout()
 
 	if (CountdownSeconds > 0) 
 	{
-		UE_LOG(LogTemp, Display, TEXT("Countdown: %d"), CountdownSeconds);
+		
+		ScreenMessageWidget->SetMessageText(FString::FromInt(CountdownSeconds));
 	}
 	else if (CountdownSeconds == 0) 
 	{
-		UE_LOG(LogTemp, Display, TEXT("GO!"));
+		ScreenMessageWidget->SetMessageText("Go!");
 		Tank->SetPlayerEnabledState(true);
 	}
 	else
 	{
 		GetWorldTimerManager().ClearTimer(CountdownTimerHandle);
-		UE_LOG(LogTemp, Display, TEXT("Clear Timer"));
+		ScreenMessageWidget->SetVisibility(ESlateVisibility::Hidden);
 	}
 }
 
@@ -107,8 +117,10 @@ void ABattleBlasterGameMode::ActorDied(AActor* DeadActor)
 	if (IsGameOver) 
 	{
 		FString GameOverString = IsVictory ? "Victory" : "Defeat";
-		
 
+		ScreenMessageWidget->SetVisibility(ESlateVisibility::Visible);
+		
+		ScreenMessageWidget->SetMessageText(GameOverString);
 		FTimerHandle GameOverTimerHandle;
 		GetWorldTimerManager().SetTimer(GameOverTimerHandle, this, &ABattleBlasterGameMode::OnGameOverTimeout, GameOverDelay, false);
 	}
